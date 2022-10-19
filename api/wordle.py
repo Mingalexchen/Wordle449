8d4189cda22b6121f3d35058e1b32426dbcb6032
Mingalexchen <54679891+Mingalexchen@users.noreply.github.com>
2022-10-18 21:12:41 -0700

    Add files via upload

--- api/wordle.py
+++ api/wordle.py
@@ -4,6 +4,7 @@
 # <https://programminghistorian.org/en/lessons/creating-apis-with-python-and-flask>.
 #
 
+from pickle import NONE
 import collections
 import dataclasses
 import sqlite3
@@ -11,6 +12,9 @@ import textwrap
 
 import databases
 import toml
+import json
+
+from quart_schema import validate_request
 
 from quart import Quart, g, request, abort
 from quart_schema import QuartSchema, RequestSchemaValidationError, validate_request
@@ -46,6 +50,15 @@ class SecretWords:
 #############################################
 
 
+#############---helper functions---#############
+
+#############################################
+
+
+
+
+
+
 #############---DB connection---#############
 async def _connect_db():
     database = databases.Database(app.config["DATABASES"]["URL"])
@@ -57,7 +70,6 @@ def _get_db():
     if not hasattr(g, "sqlite_db"):
         g.sqlite_db = _connect_db()
     return g.sqlite_db
-
 
 @app.teardown_appcontext
 async def close_connection(exception):
@@ -99,16 +111,203 @@ async def auth():
 
 
 #############---user games---#############
-'''tbd
-@app.route("user/games/info", methods=["GET"])
-async def all_games():
+#---returns all games played by the user
+@app.route("/<int:id>/games", methods=["GET"])
+async def user_games(id):
     db = await _get_db()
-    all_game = await db.fetch_all("SELECT * FROM GameStats;")
+    all_game = await db.fetch_all("SELECT * FROM GameStats WHERE user_id = :id;", values={"id":id})
+    if all_game:
+        return list(map(dict, all_game))
+    else:
+        abort(404)
 
-    return list(map(dict, all_game))
-'''
+#tbd: user who have not played any game
 
 ##############################################
+
+
+
+
+#############---answer attempt block---#############
+#
+#############---compare string function---#############
+#--- compare two 5 char words  answer  and  attempt
+#--- and returns a json in formate {str1,str2,str3,str4,str5}
+#--- where stri = "correct" if attempt[i] = answer[i]
+# ---           = "almost there"  if attempt[i] != answer[i]
+# ---                                && attempt[i] exist in answer  
+# ---           = "incorrect" if word[i] not exist in answer 
+async def compare_answer_str(attempt, answer):
+    result_dict = {0:"error",1:"error",2:"error",3:"error",4:"error"}
+    for i in range(5):
+        if attempt[i] == answer[i]:
+            result_dict[i] = "correct"
+        elif attempt[i] in answer:
+            result_dict[i] = "valid"
+        else: 
+            result_dict[i] = "incorrect"
+    
+    result_json = json.dumps(result_dict)
+    return result_json
+#
+#############---check range function---#############
+#--- a function that returns game_result in GameStats
+async def get_game_result(game_id):
+    db = await _get_db()
+    game_result = await db.fetch_one("SELECT game_result FROM GameStats WHERE game_id = :id;", values={"id":game_id}) 
+    return game_result
+#
+#############---check attempted function---#############
+#--- a function that returns answer_attempted in GameStats
+async def get_attempt_number(game_id):
+    db = await _get_db()
+    answer_attempts = await db.fetch_one("SELECT answer_attempted FROM GameStats WHERE game_id = :id;", values={"id":game_id}) 
+    return answer_attempts
+#
+#############---insert attempt function---#############
+#--- a function that insert user entry to GameStats attempt_i
+async def enter_answer(game_id,user_entry,attempt_number):
+    db = await _get_db()
+    if attempt_number == 0:
+        insert_content = {'attempt_1': user_entry, 'game_id': game_id}    
+        insert_content = dict(insert_content)
+        try:
+            await db.execute(
+            '''
+            UPDATE GameStats 
+            SET attempt_1 = :attempt_1
+            WHERE game_id = :game_id;
+            ''',
+            insert_content
+        )
+        except sqlite3.IntegrityError as e:
+            abort(409,e)
+        return {"1st attempt": "finished"}
+    elif attempt_number == 1:
+        insert_content = {'attempt_2': user_entry, 'game_id': game_id}    
+        insert_content = dict(insert_content)
+        try:
+            await db.execute(
+            '''
+            UPDATE GameStats 
+            SET attempt_2 = :attempt_2
+            WHERE game_id = :game_id;
+            ''',
+            insert_content
+        )
+        except sqlite3.IntegrityError as e:
+            abort(409,e)
+        return {"2nd attempt": "finished"}
+    elif attempt_number == 2:
+        insert_content = {'attempt_3': user_entry, 'game_id': game_id}    
+        insert_content = dict(insert_content)
+        try:
+            await db.execute(
+            '''
+            UPDATE GameStats 
+            SET attempt_3 = :attempt_3
+            WHERE game_id = :game_id;
+            ''',
+            insert_content
+        )
+        except sqlite3.IntegrityError as e:
+            abort(409,e)
+        return {"3rd attempt": "finished"}
+    elif attempt_number == 3:
+        insert_content = {'attempt_4': user_entry, 'game_id': game_id}    
+        insert_content = dict(insert_content)
+        try:
+            await db.execute(
+            '''
+            UPDATE GameStats 
+            SET attempt_4 = :attempt_4
+            WHERE game_id = :game_id;
+            ''',
+            insert_content
+        )
+        except sqlite3.IntegrityError as e:
+            abort(409,e)
+        return {"4th attempt": "finished"}
+    elif attempt_number == 4:
+        insert_content = {'attempt_5': user_entry, 'game_id': game_id}    
+        insert_content = dict(insert_content)
+        try:
+            await db.execute(
+            '''
+            UPDATE GameStats 
+            SET attempt_5 = :attempt_5
+            WHERE game_id = :game_id;
+            ''',
+            insert_content
+        )
+        except sqlite3.IntegrityError as e:
+            abort(409,e)
+        return {"5th attempt": "finished"}
+    elif attempt_number == 5:
+        insert_content = {'attempt_6': user_entry, 'game_id': game_id}    
+        insert_content = dict(insert_content)
+        try:
+            await db.execute(
+            '''
+            UPDATE GameStats 
+            SET attempt_6 = :attempt_6
+            WHERE game_id = :game_id;
+            ''',
+            insert_content
+        )
+        except sqlite3.IntegrityError as e:
+            abort(409,e)
+        return {"6th attempt": "finished"}
+    else:
+        return {"attempt": "failed"}
+ 
+#
+#########---update answer_attempted function---#########
+#--- a function that increases answer_attempted by 1
+async def increase_attempt_number(game_id):
+    db = await _get_db()
+    game_id_dict = {'game_id': game_id} 
+    try:
+        await db.execute(
+        '''
+        UPDATE GameStats SET answer_attempted = answer_attempted + 1
+        WHERE game_id = :game_id;
+        ''',
+        game_id_dict
+    )
+    except sqlite3.IntegrityError as e:
+        abort(409,e)
+    return {"attempt number": "updated"}
+#
+#############---respond to answer---#############
+@app.route("/user/games/<int:game_id>", methods=["POST"])
+async def respond_to_an_entry(game_id):
+    if get_game_result(game_id) != 0:  #check if game has ended
+        abort(400)     #return {"Attempt failed":"Game already ended!"}
+    data = await request.get_json()  #receive user entry attempt 
+    if not data:                    #check if received data
+        abort(404)
+    attempt = f"{data['attempt']}"  #convert user entry to string
+    if attempt.len() != 5:  #if attempt not a length 5 word, return error
+        abort(409)  
+    db = await _get_db()   #connect to DB
+    attempt_number = get_attempt_number(game_id)  
+    if attempt_number > 5:      #check if the user have less than 6 answers
+        abort(400)
+    secret_word = await db.fetch_one("SELECT secret_word FROM GameStats WHERE game_id = :id;", values={"id":game_id}) 
+    if not secret_word:     #if get secret_word fail return 400
+        abort(400)
+    answer = f'{secret_word}'
+    return_json = await compare_answer_str(attempt, answer)  #compare user entry with secret word    
+    enter_answer(game_id, attempt, attempt_number)
+    increase_attempt_number(game_id)
+    return return_json
+   
+
+##############################################
+
+
+
 
 
 
@@ -138,7 +337,184 @@ async def all_words():
 
 
 
-#############################################
+
+
+
+
+
+#############---new game---#############
+@app.route("/<int:user_id>/newgame", methods=["POST"])
+async def new_game(user_id):
+    db = await _get_db()
+###-------!!!!!!!ERROR MESSAGE 
+###-------!!!!!!!RANDOM HAS ERROR
+    new_secret_word = await db.execute(
+        '''
+        SELECT * FROM SecretWords ORDER BY RANDOM() LIMIT 1;
+        ''')
+    new_secret_word = f"{new_secret_word}"
+    insert_content = {'game_id': None, 'user_id': user_id, 'game_result':None,
+            'answer_attempted':0, 'secret_word':new_secret_word,
+            'attempt_1':None, 'attempt_2':None, 'attempt_3':None,
+            'attempt_4':None, 'attempt_5':None, 'attempt_6':None}    
+    insert_content = dict(insert_content)
+
+    try:
+        await db.execute(
+            '''
+            INSERT INTO GameStats(game_id, user_id, game_result,
+            answer_attempted, secret_word,
+            attempt_1, attempt_2, attempt_3,
+            attempt_4, attempt_5, attempt_6)
+            VALUES(:game_id,:user_id, :game_result,
+            :answer_attempted, :secret_word,
+            :attempt_1, :attempt_2, :attempt_3,
+            :attempt_4, :attempt_5, :attempt_6);
+            ''',
+            insert_content
+        )
+    except sqlite3.IntegrityError as e:
+        abort(409,e)
+    return {"New Game Started": "success"}
+
+##############################################
+
+
+
+
+
+#############---modify game helper---#############
+@app.route("/<int:game_id>/increaseattempt", methods=["PUT"])
+async def increase_attempt_val(game_id):
+    db = await _get_db()
+    game_id_dict = {'game_id': game_id} 
+    try:
+        await db.execute(
+        '''
+        UPDATE GameStats SET answer_attempted = answer_attempted + 1
+        WHERE game_id = :game_id;
+        ''',
+        game_id_dict
+    )
+    except sqlite3.IntegrityError as e:
+        abort(409,e)
+    return {"update": "success"}
+
+
+
+
+
+#############---game update---#############
+@app.route("/<int:game_id>/newmove", methods=["PUT"])
+async def update_game(game_id):
+    db = await _get_db()
+
+    data = await request.get_json()
+    user_entry = f"{data['entry']}"
+    attempt_number = int(f"{data['attempt_number']}")
+    if attempt_number == 0:
+        insert_content = {'attempt_1': user_entry, 'game_id': game_id}    
+        insert_content = dict(insert_content)
+        try:
+            await db.execute(
+            '''
+            UPDATE GameStats 
+            SET attempt_1 = :attempt_1
+            WHERE game_id = :game_id;
+            ''',
+            insert_content
+        )
+        except sqlite3.IntegrityError as e:
+            abort(409,e)
+        return {"1st attempt": "finished"}
+    elif attempt_number == 1:
+        insert_content = {'attempt_2': user_entry, 'game_id': game_id}    
+        insert_content = dict(insert_content)
+        try:
+            await db.execute(
+            '''
+            UPDATE GameStats 
+            SET attempt_2 = :attempt_2
+            WHERE game_id = :game_id;
+            ''',
+            insert_content
+        )
+        except sqlite3.IntegrityError as e:
+            abort(409,e)
+        return {"2nd attempt": "finished"}
+    elif attempt_number == 2:
+        insert_content = {'attempt_3': user_entry, 'game_id': game_id}    
+        insert_content = dict(insert_content)
+        try:
+            await db.execute(
+            '''
+            UPDATE GameStats 
+            SET attempt_3 = :attempt_3
+            WHERE game_id = :game_id;
+            ''',
+            insert_content
+        )
+        except sqlite3.IntegrityError as e:
+            abort(409,e)
+        return {"3rd attempt": "finished"}
+    elif attempt_number == 3:
+        insert_content = {'attempt_4': user_entry, 'game_id': game_id}    
+        insert_content = dict(insert_content)
+        try:
+            await db.execute(
+            '''
+            UPDATE GameStats 
+            SET attempt_4 = :attempt_4
+            WHERE game_id = :game_id;
+            ''',
+            insert_content
+        )
+        except sqlite3.IntegrityError as e:
+            abort(409,e)
+        return {"4th attempt": "finished"}
+    elif attempt_number == 4:
+        insert_content = {'attempt_5': user_entry, 'game_id': game_id}    
+        insert_content = dict(insert_content)
+        try:
+            await db.execute(
+            '''
+            UPDATE GameStats 
+            SET attempt_5 = :attempt_5
+            WHERE game_id = :game_id;
+            ''',
+            insert_content
+        )
+        except sqlite3.IntegrityError as e:
+            abort(409,e)
+        return {"5th attempt": "finished"}
+    elif attempt_number == 5:
+        insert_content = {'attempt_6': user_entry, 'game_id': game_id}    
+        insert_content = dict(insert_content)
+        try:
+            await db.execute(
+            '''
+            UPDATE GameStats 
+            SET attempt_6 = :attempt_6
+            WHERE game_id = :game_id;
+            ''',
+            insert_content
+        )
+        except sqlite3.IntegrityError as e:
+            abort(409,e)
+        return {"6th attempt": "finished"}
+    else:
+        return {"attempt": "failed"}
+##############################################
+
+
+
+
+
+
+
+
+
+
 #############---error handlers---#############
 @app.errorhandler(404)
 def not_found(e):
@@ -160,8 +536,15 @@ def conflict(e):
 
 
 
-
 '''
+dictA = json.loads('{"6th attempt": "finished"}')
+        dictB = json.loads('{"entry": "finished"}')
+        merged_dict = {**dictA, **dictB}
+        jsonString_merged = json.dumps(merged_dict)
+
+
+
+
 @app.route("/books/all", methods=["GET"])
 async def all_books():
     db = await _get_db()
